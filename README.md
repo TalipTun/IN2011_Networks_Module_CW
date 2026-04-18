e3d72a1e58102349f3d9fcb6686a094e4a8a414f
TalipTun <taliptun62@gmail.com>
2026-04-18 22:24:27 +0100

    testing commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# IN2011_Networks_Module_CW
-
-Hello!
-
-This file explains how my coursework project works
-
-test commit
--- src/HashID.java
+++ src/HashID.java
@@ -11,6 +11,9 @@ public class HashID {
     public static byte [] computeHashID(String s) throws Exception {
 	// What this does and how it works is covered in a later lecture
 	MessageDigest md = MessageDigest.getInstance("SHA-256");
+	System.err.println("1");
+	System.err.println("2");
+	System.err.println("3");
 	md.update(s.getBytes(StandardCharsets.UTF_8));
 	return md.digest();
     }
