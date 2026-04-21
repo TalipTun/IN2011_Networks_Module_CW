264059f675bf0baab1ec74193ac6460de5a014f9
TalipTun <taliptun62@gmail.com>
2026-04-21 02:24:47 +0100

    test(personal): update UDP manual test runner for message handling

--- src/PersonalTest.java
+++ src/PersonalTest.java
@@ -1,7 +1,8 @@
 class PersonalTest {
     public static void main(String[] args) throws Exception {
         Node testNode = new Node();
+        testNode.setNodeName("N:test0");
         testNode.openPort(12345);
-        testNode.handleIncomingMessages(10000);
+        testNode.handleIncomingMessages(0);
     }
 }
